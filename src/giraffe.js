f212728a15debe753f3b397703557b7f6668e30a
seabassgonzalez <seabassgonzalez@gmail.com>
2015-11-11 23:30:36 -0800

    Begin to add functionality

--- src/giraffe.js
+++ src/giraffe.js
@@ -1,23 +1,34 @@
-var giraffeMaker = function(name, height) {
+var giraffeMaker = function(name, height, age, hairStyle) {
   var newGiraffe = {};
   newGiraffe.name = name;
   newGiraffe.height = height;
+  newGiraffe.age = age;
+  newGiraffe.hairStyle = hairStyle;
   newGiraffe.hunger = 10;
+  newGiraffe.isDopeEnough; 
+  newGiraffe.dopeHairCut = "The Mr. T";
 
   newGiraffe.isTallEnough = function(treeHeight) {
     return newGiraffe.height > treeHeight;
   };
 
-  newGiraffe.isHungry = function() {
+ newGiraffe.isHungry = function() {
     return newGiraffe.hunger > 0;
   };
 
+ newGiraffe.isTooHipToQuit = function(age, hairStyle){
+    if((age < 42) && (hairStyle == dopeHairCut){
+	   return newGiraffe.isDopeEnough = true;
+  };
+ 
   newGiraffe.say = function(option) {
     var sentences = {
       'greet': 'Hello, my name is ' + newGiraffe.name + ', it is nice to meet you.',
       'notHungry': newGiraffe.name + ' is not hungry.',
       'notTallEnough': newGiraffe.name + ' is too short to reach the trees.',
-      'ate': 'That was delicious!'
+      'isASquare': newGiraffe.name + ' is a square.',
+	 'isDope': newGiraffe.name + ' is Too Hip to Quit!', 
+	 'ate': 'That was delicious!'
     };
 
     return console.log(sentences[option]);
@@ -40,5 +51,17 @@ var giraffeMaker = function(name, height) {
     }
   };
 
+  newGiraffe.kickBack = function() {
+    if (newGiraffe.isTooHipToQuit()){
+    }
+  };
+
+  newGiraffe.cruise = function(){
+    if (newGiraffe.isDopeEnough()){
+      newGiraffe.
+    } else {
+
+    }
+  };
   return newGiraffe;
 };
